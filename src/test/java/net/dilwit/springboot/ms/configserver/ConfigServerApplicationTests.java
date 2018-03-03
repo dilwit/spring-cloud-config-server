86fa6bdd95a3126977343cfb085c4927e84df605
Dilusha Withanage <dilusha.withanage@gmail.com>
2018-03-03 20:37:42 +1100

    Initial commit

---
+++ src/test/java/net/dilwit/springboot/ms/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,18 @@
+package net.dilwit.springboot.ms.configserver;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
+public class ConfigServerApplicationTests {
+
+	@Test
+	@Ignore
+	public void contextLoads() {
+	}
+
+}
